35048591ee3b397dae1bf6b2f467677fdfceb0d1
Taylor Phillips <taylorphillips133@gmail.com>
2020-06-25 15:38:34 -0700

    add our countSyllables function with for loops to iterate through each line
    Co-Authored-By: Noel Kirkland <noelkirkland@gmail.com>

--- src/haiku-checker.js
+++ src/haiku-checker.js
@@ -4,14 +4,26 @@ export function Haiku(line1, line2, line3) {
   this.line3 = line3;
 }
 
-Haiku.prototype.countSyllables = function(line1, line2, line3) {
+const vowel = ["a", "e", "i", "o", "u", "y"];
+
+Haiku.prototype.countSyllables = function() {
   let lineOneVowelCount = 0;
-  for(index=1; index<=line1.length; index++){
-    if ((i) === vowel){
-      lineOneVowelCount +;
+  for(let i=1; i<=this.line1.length; i++){
+    if ([i] === vowel){
+      return lineOneVowelCount += 1;
     }
   }
   let lineTwoVowelCount = 0;
+  for(let i=1; i<=this.line2.length; i++){
+    if ([i] === vowel){
+      return lineTwoVowelCount += 1;
+    }
+  }
   let lineThreeVowelCount = 0;
+  for(let i=1; i<=this.line3.length; i++){
+    if ([i] === vowel){
+      return lineThreeVowelCount += 1;
+    }
+  }
 };
 
